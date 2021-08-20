7d6fea4214e13ebe64f6612f5e09f68045967b02
MatheusHenrique18 <matheus.hsb18@gmail.com>
2021-08-20 19:02:05 -0300

    Ajustando demonstrativo de chamados, incluindo valores reais.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,3 +1,6 @@
+import { ResponseApi } from 'src/app/model/response-api';
+import { Contadores } from 'src/app/model/contadores.model';
+import { ChamadoService } from 'src/app/services/chamado.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  contadores: Contadores = new Contadores();
+  message: {};
+  classCss: {};
 
-  ngOnInit(): void {
+  constructor( 
+    private chamadoService: ChamadoService
+  ) { }
+
+  ngOnInit(){
+    this.chamadoService.listarContadores().subscribe((responseAPI: ResponseApi) => {
+      this.contadores = responseAPI.data;
+    }, err => {
+      this.showMessage({
+        type: 'error',
+        text: err['error']['errors'][0]
+      });
+    });
+  }
+
+  private showMessage(message: { type: string, text: string }): void {
+    this.message = message;
+    this.buildClasses(message.type);
+    setTimeout(() => {
+      this.message = undefined;
+    }, 5000);
+  }
+
+  private buildClasses(type: string): void {
+    this.classCss = {
+      'alert': true
+    }
+    this.classCss['alert-' + type] = true;
   }
 
 }
